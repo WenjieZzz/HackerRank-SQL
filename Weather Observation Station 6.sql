378bc1c8f264f32747c2c7499ee0fdd12b4c5d1c
WZ <44674702+WenjieZzz@users.noreply.github.com>
2019-09-25 17:03:57 -0400

    Update Weather Observation Station 6.sql

--- Weather Observation Station 6.sql
+++ Weather Observation Station 6.sql
@@ -9,3 +9,23 @@ OR CITY LIKE 'E%'
 OR CITY LIKE 'I%' 
 OR CITY LIKE 'O%' 
 OR CITY LIKE 'U%';
+
+-- Better Solution from  
+-- Author: Rodney Shaghoulian
+-- Github: github.com/RodneyShag
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[aeiou]';
+
+-- REGEXP/RLIKE: Regular expression pattern matches
+|Pattern | What the pattern matches |
+|--- | --- |
+|^ | Beginning |
+| $ | End |
+| [...]	| Any character listed between the square brackets |
+[^...]	Any character not listed between the square brackets
+p1|p2|p3	Alternation; matches any of the patterns p1, p2, or p3
+*	Zero or more instances of preceding element
++	One or more instances of preceding element
+{n}	n instances of preceding element
+{m,n}	m through n instances of preceding element
