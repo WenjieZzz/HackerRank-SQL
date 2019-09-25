05798a051dfa2a21aec527e330caeadbf815d7d5
WZ <44674702+WenjieZzz@users.noreply.github.com>
2019-09-25 17:05:36 -0400

    Update Weather Observation Station 6.sql

--- Weather Observation Station 6.sql
+++ Weather Observation Station 6.sql
@@ -17,20 +17,5 @@ OR CITY LIKE 'U%';
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[aeiou]';
 
-| Command | Description |
-| --- | --- |
-| git status | List all new or modified files |
-| git diff | Show file differences that haven't been staged |
-
 -- REGEXP/RLIKE: Regular expression pattern matches
-|Pattern | What the pattern matches |
-|--- | --- |
-|^ | Beginning |
-| $ | End |
-| [...]	| Any character listed between the square brackets |
-[^...]	Any character not listed between the square brackets
-p1|p2|p3	Alternation; matches any of the patterns p1, p2, or p3
-*	Zero or more instances of preceding element
-+	One or more instances of preceding element
-{n}	n instances of preceding element
-{m,n}	m through n instances of preceding element
+Ref: https://www.tutorialspoint.com/mysql/mysql-regexps.htm
